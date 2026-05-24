08f506980c13370d35886dbdd695ec65eb15d83d
gabrieltol7do <gabrieltoledo4629@gmail.com>
2026-05-24 20:35:46 -0300

    ajustes finais

--- js/script.js
+++ js/script.js
@@ -1,48 +1,69 @@
-alert("JS carregado");
-
-document.addEventListener("DOMContentLoaded", function () {
-
-  const loginForm = document.getElementById("loginForm");
-
-  alert("Form encontrado");
-
-  loginForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    alert("Submit funcionando");
-
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-
-    if (email === "admin@tereverde.com" && senha === "123") {
-      alert("Login realizado com sucesso!");
-      window.location.href = "admin.html";
-    } else {
-      alert("E-mail ou senha inválidos.");
-    }
-  });
-
-});
-
 function alterarStatus() {
+
   const status = document.getElementById("statusParque");
 
   if (status.innerText.trim() === "Aberto") {
-    status.innerText = "Fechado";
-    status.className = "status fechado";
-  } else {
-    status.innerText = "Aberto";
-    status.className = "status aberto";
-  }
 
-  alert("Status atualizado com sucesso!");
+    status.innerText = "Fechado";
+
+    status.classList.remove("aberto");
+    status.classList.add("fechado");
+
+  } else {
+
+    status.innerText = "Aberto";
+
+    status.classList.remove("fechado");
+    status.classList.add("aberto");
+  }
 }
+
 function abrirModal() {
+
   document.getElementById("modalEvento").style.display = "flex";
 }
 
 function fecharModal() {
-  document.getElementById("modalEvento").style.display = "none";
 
-  alert("Evento criado com sucesso!");
+  document.getElementById("modalEvento").style.display = "none";
+}
+
+function emitirAlerta() {
+
+  alert("Alerta ambiental emitido com sucesso.");
+}
+
+function atualizarHorarios() {
+
+  alert("Horários dos parques atualizados.");
+}
+
+function gerarRelatorio() {
+
+  alert("Relatório ambiental gerado.");
+}
+
+const form = document.getElementById("loginForm");
+
+if(form){
+
+  form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    if(email === "admin@teste.com" && senha === "1234"){
+
+      window.location.href = "admin.html";
+
+    } else {
+
+      alert("E-mail ou senha inválidos.");
+
+    }
+
+  });
+
 }
